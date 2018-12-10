fefcff84e5aac180540c0240cc3b6ffc9deee97d
OrionStark <robbybellamy6@gmail.com>
2018-12-10 10:57:28 +0700

    Iris blueprint update

--- blueprints/IrisBlueprint.py
+++ blueprints/IrisBlueprint.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, Response
 import numpy as np
 import pandas as pd
 import os
-import json
 
 ## GETTING THE DATASET PATH
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -18,4 +17,24 @@ def getDataSet():
     iris_data = []
     for i in range(len(data.values)):
         iris_data.append({ 'sepal-length': data.values[i][0], 'sepal-width': data.values[i][1], 'petal-length': data.values[i][2], 'petal-width': data.values[i][3], 'class-name': data.values[i][4] })
-    return Response(json.dumps(iris_data), mimetype='application/json')
+    return jsonify(iris_data)
+
+## ROUTE TO GET IRIS SETOSA DATASET
+@IrisBlueprint.route('/iris/dataset/iris-setosa')
+def getIrisSetosaData():
+    data = pd.read_csv(DATASET_PATH)
+    iris_data = []
+    for i in range(len(data.values)):
+        if ( data.values[i][4] == 'Iris-setosa' ):
+            iris_data.append({ 'sepal-length': data.values[i][0], 'sepal-width': data.values[i][1], 'petal-length': data.values[i][2], 'petal-width': data.values[i][3], 'class-name': data.values[i][4] })
+    return jsonify(iris_data)
+
+## ROUTE TO GET IRIS VERSICOLOR
+@IrisBlueprint.route('/iris/dataset/iris-versicolor')
+def getIrisVersiColorData():
+    data = pd.read_csv(DATASET_PATH)
+    iris_data = []
+    for i in range(len(data.values)):
+        if ( data.values[i][4] == 'Iris-versicolor' ):
+            iris_data.append({ 'sepal-length': data.values[i][0], 'sepal-width': data.values[i][1], 'petal-length': data.values[i][2], 'petal-width': data.values[i][3], 'class-name': data.values[i][4] })
+    return jsonify(iris_data) 
